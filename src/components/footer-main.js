d6906b2876c0fc98ecfe520481bc3bb74043e98e
Prabir Singh <codeprabir@gmail.com>
2022-09-20 01:58:10 +0530

    react project practice

---
+++ src/components/footer-main.js
@@ -0,0 +1,11 @@
+import './footer-main.css'
+
+function Footer() {
+  return (
+    <div className="footer-main">
+      <small>© 2022 xyz development. All rights reserved.</small>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/header-main.js
@@ -0,0 +1,17 @@
+import './header-main.css'
+
+function Header() {
+  return (
+    <header className='header-main'>
+      <img src='./logo192.png' alt='logo' />
+      <nav>
+        <ul>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+export default Header
